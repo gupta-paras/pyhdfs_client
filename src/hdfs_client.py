450f952616ee4c3b4bebb830145c81ef56518632
Paras Gupta <parasgupta1096@gmail.com>
2021-03-22 02:30:11 +0530

    Added Main Script

--- src/hdfs_client.py
+++ src/hdfs_client.py
@@ -1,16 +1,100 @@
-from py4j.java_gateway import JavaGateway, GatewayParameters
+import os
+import re
+
+from py4j.java_gateway import launch_gateway, JavaGateway, GatewayParameters, CallbackServerParameters, java_import
 
 class HDFSClient:
+    CLASSPATH ="""
+        {HADOOP_HOME}\\etc\\hadoop;{HADOOP_HOME}\\share\\hadoop\\common\\lib\\*;
+        {HADOOP_HOME}\\share\\hadoop\\common\\*;
+        {HADOOP_HOME}\\share\\hadoop\\hdfs;
+        {HADOOP_HOME}\\share\\hadoop\\hdfs\\lib\\*;
+        {HADOOP_HOME}\\share\\hadoop\\hdfs\\*;
+        {HADOOP_HOME}\\share\\hadoop\\yarn;{HADOOP_HOME}\\share\\hadoop\\yarn\\lib\\*;
+        {HADOOP_HOME}\\share\\hadoop\\yarn\\*;
+        {HADOOP_HOME}\\share\\hadoop\\mapreduce\\lib\\*;
+        {HADOOP_HOME}\\share\\hadoop\\mapreduce\\*;
+        {JAVA_HOME}\\*
+    """
+
     def __init__(self, **kwargs):
-        port = kwargs.get('port', 25333)
-        self.gateway = self.launch_gateway()
-        self.shell = self.gateway.entry_point.GetWrapper()
+        self.classpath = HDFSClient.CLASSPATH.format(
+            HADOOP_HOME=os.environ['HADOOP_HOME'], JAVA_HOME = os.environ['JAVA_HOME']
+        )
+        self.classpath = re.sub('\s+', '', self.classpath)
+        self.log_file_basepath = kwargs.get('log_file_basepath', '.')
+        
+        self.update_hadoop_environ()
+        self.launch_gateway()
+        self.launch_hdfs_shell()
+
+    def update_hadoop_environ(self):
+        os.environ['HADOOP_CONF_DIR'] = os.path.join(os.environ['HADOOP_HOME'], r'\etc\hadoop')
+        os.environ['YARN_CONF_DIR'] = os.environ['HADOOP_HOME']
+        os.environ['HADOOP_PREFIX'] = os.environ['HADOOP_HOME']
+        os.environ['PATH'] = os.environ['PATH'] + ";" + os.path.join(os.environ['HADOOP_HOME'], r'\bin')
+
+    def update_log_files(self):
+        self.out_file = os.path.join(self.log_file_basepath, 'out.log')
+        self.err_file = os.path.join(self.log_file_basepath, 'err.log')
+        self.out = open(self.out_file, 'w')
+        self.err = open(self.err_file, 'w')
 
     def launch_gateway(self):
-        raise NotImplementedError
 
-    def dfs_execute(self, args):
-        jvm_array = self.gateway.new_array(self.gateway.jvm.java.lang.String, len(args))
-        for idx, arg in enumerate(args):
-            jvm_array[idx] = arg
-        # capture stdout, stderr
+        self.update_log_files()
+        port = launch_gateway(
+            classpath=self.classpath,
+            redirect_stderr=self.err,
+            redirect_stdout=self.out,
+            die_on_exit=True
+        )
+        
+        self.gateway = JavaGateway(
+            gateway_parameters=GatewayParameters(port=port),
+            callback_server_parameters=CallbackServerParameters(port=0)
+        )
+
+        python_port = self.gateway.get_callback_server().get_listening_port()
+
+        self.gateway.java_gateway_server.resetCallbackClient(
+            self.gateway.java_gateway_server.getCallbackClient().getAddress(),
+            python_port
+        )
+
+    def launch_hdfs_shell(self):
+        java_import(self.gateway.jvm, 'org.apache.hadoop.fs.FsShell')
+        java_import(self.gateway.jvm, 'org.apache.hadoop.conf.Configuration')
+        self.fsshell = self.gateway.jvm.FsShell()
+        conf = self.gateway.jvm.Configuration()
+        conf.setQuietMode(False)
+        self.fsshell.setConf(conf)
+
+    def get_java_array(self, py_list):
+        java_list = self.gateway.new_array(self.gateway.jvm.java.lang.String,len(py_list))
+        for idx in range(len(py_list)):
+            java_list[idx] = py_list[idx]
+        return java_list
+
+    def run(self, cmd):
+
+        self.err.seek(0)
+        self.err.truncate()
+
+        print_stream = self.gateway.jvm.java.io.PrintStream(self.out_file)
+        self.gateway.jvm.System.setOut(print_stream)
+        ret = self.fsshell.run(hdfs_client.get_java_array(cmd))
+        print_stream.close()
+
+
+        with open(self.out_file) as f:
+            out = f.read()
+        with open(self.err_file) as f:
+            err = f.read()
+        return ret, out, err
+
+if __name__=='__main__':
+    os.environ['HADOOP_HOME'] = r'C:\bin\hadoop\hadoop-2.9.2'
+    os.environ['JAVA_HOME'] = r'C:\Progra~1\Java\jdk1.8.0_91'
+    hdfs_client = HDFSClient()
+    print(hdfs_client.run(['-ls', '/folder1']))
